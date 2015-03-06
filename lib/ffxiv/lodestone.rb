842ebcd5e77dc5034eba41c4e4227879422ceb9f
Isjaki Kveikur <isjaki.xiv@gmail.com>
2015-03-06 14:38:58 -0800

    Add debug option
    If debug mode is enabled, #d(str) will output debug message str.

--- lib/ffxiv/lodestone.rb
+++ lib/ffxiv/lodestone.rb
@@ -8,17 +8,37 @@ require "open-uri"
 require "lodestone/model"
 require "lodestone/character"
 require "lodestone/free-company"
+require "lodestone/linkshell"
 
 module FFXIV
   module Lodestone
+
+    @@debug = false
+
     class << self
+
+      def debug
+        @@debug
+      end
+
+      def debug=(flag)
+        @@debug = flag
+      end
+
+      def d(str)
+        puts "\033[35m[Debug]\033[0m #{str}" if @@debug
+      end
+
       def fetch(endpoint)
         uri = "http://na.finalfantasyxiv.com/lodestone/" + endpoint
+        d("Fetching #{uri} ......")
         html, charset = open(uri) do |page|
           [page.read, page.charset]
         end
+        d("    done (size=#{html.size}, charset=#{charset})")
         Nokogiri::HTML.parse(html, nil, charset)
       end
+
     end
   end
 end
