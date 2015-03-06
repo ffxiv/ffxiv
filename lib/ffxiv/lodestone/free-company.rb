2ea39f04895d25d5011341b088ab8b7cf19c15bd
Isjaki Kveikur <isjaki.xiv@gmail.com>
2015-03-06 14:42:06 -0800

    Update scraping logic to be compatible with the new Lodestone pages

--- lib/ffxiv/lodestone/free-company.rb
+++ lib/ffxiv/lodestone/free-company.rb
@@ -9,9 +9,44 @@ module FFXIV
 
       class << self
 
+        def search(keyword, server: nil, page: 1, verbose: false)
+          dom = Lodestone.fetch("freecompany/?q=#{URI.escape(keyword)}&worldname=#{URI.escape(server)}&page=#{URI.escape(page.to_s)}")
+          free_companies = dom.search("table.table_elements_com_fc tr").map do |tr|
+            h4 = tr.at("td h4")
+            a = h4.at("a")
+            id = a.attr("href").split("/")[-1].to_i
+            if verbose
+              self.find_by_id(id)
+            else
+              self.new({
+                id: id,
+                name: a.content,
+                server: h4.at("span").content.strip[1...-1],
+                grand_company: tr.at("div.groundcompany_name").content
+              })
+            end
+          end
+          pagination = dom.at("div.current_list")
+          total = pagination.at("span.total").content.to_i
+          results = {
+            show_start: pagination.at("span.show_start").content.to_i,
+            show_end: pagination.at("span.show_end").content.to_i,
+            total: total,
+            num_pages: (total / 50.0).ceil,
+            free_companies: free_companies
+          }
+        end
+
         def name_to_id(name, server)
           dom = Lodestone.fetch("freecompany/?q=#{URI.escape(name)}&worldname=#{URI.escape(server)}")
           dom.at("div.ic_freecompany_box a").attr("href").split("/")[-1].to_i
+        end
+
+        def name_to_id(name, server)
+          self.search(name, server: server)[:free_companies].each do |fc|
+            return fc.id if name == fc.name
+          end
+          nil
         end
 
         def find_by_id(id)
@@ -21,7 +56,7 @@ module FFXIV
             props = {}
             props[:id] = id
 
-            props[:logo_uri] = dom.css("div.ic_crest_64 img").to_a.map{|n| n.attr("src")}
+            props[:logo_uri] = dom.search("div.ic_crest_64 img").map{|n| n.attr("src")}
 
             gcs_node = dom.at("span.friendship_color")
             props[:grand_company_standing] = gcs_node.inner_text[1...-1]
@@ -37,7 +72,7 @@ module FFXIV
 
             props[:tag] = dom.at("td.vm").last_element_child.next_sibling.inner_text[1...-1]
 
-            dom.css("table.table_style2 tr").to_a.each do |tr|
+            dom.search("table.table_style2 tr").each do |tr|
               td_node = tr.at("td")
               td_text = td_node.inner_text.strip
               case tr.at("th").inner_text
@@ -52,25 +87,25 @@ module FFXIV
                   props[:weekly_rank] = weekly.include?("--") ? nil : weekly.match(/\d+/)[0].to_i
                   props[:monthly_rank] = monthly.include?("--") ? nil : monthly.match(/\d+/)[0].to_i
                 when "Company Slogan"
-                  props[:slogan] = td_text
+                  props[:slogan] = td_text == "" ? nil : td_text
                 when "Active"
-                  props[:active] = td_text
+                  props[:active] = td_text == "Not specified" ? nil : td_text
                 when "Recruitment"
                   props[:recruiting] = td_text == "Open"
                 when "Focus"
-                  props[:focus] = td_node.css("img").to_a.map{|n| n.attr("title")}
+                  props[:focus] = td_node.search("li:not(.icon_off) img").map{|n| n.attr("title")}
                 when "Seeking"
-                  props[:seeking] = td_node.css("img").to_a.map{|n| n.attr("title")}
+                  props[:seeking] = td_node.search("li:not(.icon_off) img").map{|n| n.attr("title")}
                 when "Estate Profile"
                   name_node = td_node.at("div.txt_yellow")
                   if name_node
-                    address_size, greeting = td_node.css("p").to_a.map(&:inner_text)
+                    address_size, greeting = td_node.search("p").map(&:inner_text)
                     address, size = address_size.split(" (")
                     props[:housing] = {
                       name: name_node.inner_text,
                       address: address,
                       size: size[0...-1],
-                      greeting: greeting
+                      greeting: greeting == "No greeting available." ? nil : greeting
                     }
                   else
                     props[:housing] = nil
@@ -91,15 +126,15 @@ module FFXIV
           num_pages = (@num_members / 50.0).ceil # 50 members / page
           1.upto(num_pages) do |page_no|
             dom = Lodestone.fetch("freecompany/#{@id}/member/?page=#{page_no}")
-            dom.css("div.player_name_area").to_a.each do |node|
+            dom.search("div.player_name_area").each do |node|
               cid = node.at("div.name_box a").attr("href").split("/")[-1]
-              fcrank = node.at("div.fc_member_status").inner_text
+              fcrank = node.at("div.fc_member_status").content.strip
               members[cid] = fcrank
             end
           end
           characters = []
           members.each do |cid, fcrank|
-            character = Character.find(cid)
+            character = Character.find_by_id(cid)
             character.free_company_rank = fcrank.strip
             characters << character
           end
